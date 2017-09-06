bb395f5e591d49b4ac356095a45e10aa62195e9f
Alessandro Bagnoli <alessandro.bagnoli4@studio.unibo.it>
2017-09-06 17:59:41 +0200

    prima unione di progetto.

--- src/dist/srcMore/it/unibo/ctxRobot/robotsystem.pl
+++ src/dist/srcMore/it/unibo/ctxRobot/robotsystem.pl
@@ -1,8 +1,16 @@
 %====================================================================================
 % Context ctxRobot  SYSTEM-configuration: file it.unibo.ctxRobot.robotSystem.pl 
 %====================================================================================
-context(ctxrobot, "localhost",  "TCP", "8079" ).  		 
+context(ctxradar, "192.168.137.1",  "TCP", "8033" ).  		 
+context(ctxsensoremitter, "192.168.137.2",  "TCP", "8133" ).  		 
+context(ctxrobot, "192.168.137.2",  "TCP", "8079" ).  		 
 %%% -------------------------------------------
+qactor( radargui , ctxradar, "it.unibo.radargui.MsgHandle_Radargui"   ). %%store msgs 
+qactor( radargui_ctrl , ctxradar, "it.unibo.radargui.Radargui"   ). %%control-driven 
+qactor( usercmdhandler , ctxradar, "it.unibo.usercmdhandler.MsgHandle_Usercmdhandler"   ). %%store msgs 
+qactor( usercmdhandler_ctrl , ctxradar, "it.unibo.usercmdhandler.Usercmdhandler"   ). %%control-driven 
+qactor( sensorsonar , ctxsensoremitter, "it.unibo.sensorsonar.MsgHandle_Sensorsonar"   ). %%store msgs 
+qactor( sensorsonar_ctrl , ctxsensoremitter, "it.unibo.sensorsonar.Sensorsonar"   ). %%control-driven 
 %%% -------------------------------------------
 eventhandler(evh,ctxrobot,"it.unibo.ctxRobot.Evh","alarm,obstacle,sensordata").  
 %%% -------------------------------------------
