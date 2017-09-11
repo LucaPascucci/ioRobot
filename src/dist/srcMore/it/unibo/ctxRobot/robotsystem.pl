7768e3fd841e9c006dc1491f470ac1e3eafb8154
Alessandro Bagnoli <alessandro.bagnoli4@studio.unibo.it>
2017-09-11 11:03:01 +0200

    Inizio invio foto.

--- src/dist/srcMore/it/unibo/ctxRobot/robotsystem.pl
+++ src/dist/srcMore/it/unibo/ctxRobot/robotsystem.pl
@@ -7,12 +7,14 @@ context(ctxrobot, "172.20.10.14",  "TCP", "8079" ).
 %%% -------------------------------------------
 qactor( radargui , ctxradar, "it.unibo.radargui.MsgHandle_Radargui"   ). %%store msgs 
 qactor( radargui_ctrl , ctxradar, "it.unibo.radargui.Radargui"   ). %%control-driven 
-qactor( usercmdhandler , ctxradar, "it.unibo.usercmdhandler.MsgHandle_Usercmdhandler"   ). %%store msgs 
-qactor( usercmdhandler_ctrl , ctxradar, "it.unibo.usercmdhandler.Usercmdhandler"   ). %%control-driven 
+qactor( alarmhandler , ctxradar, "it.unibo.alarmhandler.MsgHandle_Alarmhandler"   ). %%store msgs 
+qactor( alarmhandler_ctrl , ctxradar, "it.unibo.alarmhandler.Alarmhandler"   ). %%control-driven 
+qactor( photoreceiver , ctxradar, "it.unibo.photoreceiver.MsgHandle_Photoreceiver"   ). %%store msgs 
+qactor( photoreceiver_ctrl , ctxradar, "it.unibo.photoreceiver.Photoreceiver"   ). %%control-driven 
 qactor( sensorsonar , ctxsensoremitter, "it.unibo.sensorsonar.MsgHandle_Sensorsonar"   ). %%store msgs 
 qactor( sensorsonar_ctrl , ctxsensoremitter, "it.unibo.sensorsonar.Sensorsonar"   ). %%control-driven 
 %%% -------------------------------------------
-eventhandler(evh,ctxrobot,"it.unibo.ctxRobot.Evh","alarm,obstacle,sensordata").  
+eventhandler(evh,ctxrobot,"it.unibo.ctxRobot.Evh","alarm,obstacle,sensordata,cmd").  
 %%% -------------------------------------------
 qactor( robot_actor , ctxrobot, "it.unibo.robot_actor.MsgHandle_Robot_actor" ). 
 qactor( robot_actor_ctrl , ctxrobot, "it.unibo.robot_actor.Robot_actor" ). 
